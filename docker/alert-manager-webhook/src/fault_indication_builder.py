5ce9f600b38eacf5f1dcdfc2a7a138950f23021c
liamkelly15-maker <liamkelly15@gmail.com>
2023-12-06 10:34:26 +0000

    latest commit

--- docker/alert-manager-webhook/src/fault_indication_builder.py
+++ docker/alert-manager-webhook/src/fault_indication_builder.py
@@ -1,12 +1,4 @@
-# --------------------------------------------------------------------------
-#  COPYRIGHT Ericsson 2022
-#
-#  The copyright to the computer program(s) herein is the property of
-#  Ericsson Inc. The programs may be used and/or copied only with written
-#  permission from Ericsson Inc. or in accordance with the terms and
-#  conditions stipulated in the agreement/contract under which the
-#  program(s) have been supplied.
-# --------------------------------------------------------------------------
+
 import datetime
 from functools import reduce
 import helper
--- docker/alert-manager-webhook/src/main.py
+++ docker/alert-manager-webhook/src/main.py
@@ -4,15 +4,6 @@ import json
 import logging
 import os
 
-# --------------------------------------------------------------------------
-#  COPYRIGHT Ericsson 2022
-#
-#  The copyright to the computer program(s) herein is the property of
-#  Ericsson Inc. The programs may be used and/or copied only with written
-#  permission from Ericsson Inc. or in accordance with the terms and
-#  conditions stipulated in the agreement/contract under which the
-#  program(s) have been supplied.
-# --------------------------------------------------------------------------
 from flask import Flask, request, abort
 from werkzeug.exceptions import HTTPException
 from alarm_handler_parser import AlarmHandlerParser
